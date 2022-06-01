a1629f1871afaba70cef424bc996744f1fafae8f
Jure Sunic <juresunic@hotmail.com>
2022-06-01 21:02:33 +0100

    Initial Commit

---
+++ DiceRoll-SwiftUI/DiceRoll_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRoll_SwiftUIApp.swift
+//  DiceRoll-SwiftUI
+//
+//  Created by Jure Šunić on 01/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct DiceRoll_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
